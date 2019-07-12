2b74481013c5f03649083e001bb59298a363545f
qcy <15544315559@163.com>
2019-07-12 19:32:57 +0800

    登录 注册路由的更改 首页的搭建

--- routes/user.js
+++ routes/user.js
@@ -5,35 +5,53 @@ const pool=require('../pool.js');
 var router=express.Router();
 //注册路由
 router.post('/reg',(req,res)=>{
-    var obj=req.body;
-    console.log(obj);
-    for(var key in obj){
-       if(!obj[key]){
-           res.send({code:400,msg:"can not be blank"})
-       }
-    }
-pool.query('insert into watch_user set ?',[obj],(err,result)=>{
-   if(err) throw err;
- 		//console.log(result);
- 		//如果数据插入成功
- 		if(result.affectedRows>0){
- 			res.send({cood:200,msg:'插入成功'});
- 		}
- 	});
+    var $uname=req.body.uname;
+    var $upwd=req.body.upwd;
+	var $uemail=req.body.email;
+ pool.query('insert into watch_user set uname=?,upwd=?,email=?',[$uname,$upwd,$uemail],(err,result)=>{
+	  if(err) throw err;
+ 	
+	  if(result.affectedRows>0){
+ 	  res.send("注册成功,请返回登录页面");
+ 	  }
+    });
+});
+
+router.get("/regname/:uname",(req,res)=>{
+	var $uname=req.params.uname;
+	pool.query(`select * from watch_user where uname=?`,[$uname],(err,result)=>{
+		if(err)throw err;
+		if(result.length>0){
+			res.send("1")
+		}else{
+			res.send("0")
+		}
+	})
 });
 
 //登录路由
 router.post('/login',(req,res)=>{
-   var obj=req.body;
-   if(!obj.uname){
-        res.send({code:400,msg:"name can not be blank"});
-        return;
-   }
-   if(!obj.upwd){
-        res.send({code:400,msg:"password can not be blank"});
-        return;
-   }
-   pool.query('select * from  watch_user where uname=? and upwd=?',[obj.uname,obj.upwd],(err,result)=>{
+//  var uname=req.body;
+//   if(!obj.uname){
+//        res.send({code:400,msg:"name can not be blank"});
+//        return;
+//   }
+//   if(!obj.upwd){
+//        res.send({code:400,msg:"password can not be blank"});
+//        return;
+//   }
+    var $uname=req.body.uname;
+	var $upwd=req.body.upwd;
+	
+	if(!$uname){
+		res.send("用户不能为空");
+		return;
+	}
+	if(!$upwd){
+		res.send("密码不能为空");
+		return;
+	}
+   pool.query('select * from  watch_user where uname=? and upwd=?',[$uname,$upwd],(err,result)=>{
        if(err) throw err;
        if(result.length>0){
            res.send({code:200,msg:'登陆成功'})
@@ -42,7 +60,6 @@ router.post('/login',(req,res)=>{
        }
    })
 })
-
 
 
 
